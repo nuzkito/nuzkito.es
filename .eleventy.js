0595a0c0983794ebf820f1097979092c584dde71
Nuzkito <nuzkito+github@gmail.com>
2025-03-18 14:37:14 +0100

    Update rss.

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,46 @@
-const markdownIt = require("markdown-it");
-const markdownItAttrs = require("markdown-it-attrs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const timeToRead = require('eleventy-plugin-time-to-read');
+const markdownIt = require("markdown-it")
+const markdownItAttrs = require("markdown-it-attrs")
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const timeToRead = require('eleventy-plugin-time-to-read')
+const config = require('./_data/config.js')
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
     const md = markdownIt({
         html: true,
         breaks: true,
         linkify: true,
     }).use(markdownItAttrs, {
         allowedAttributes: ['target']
-    }).disable('code');
+    }).disable('code')
 
-    eleventyConfig.setLibrary("md", md);
+    eleventyConfig.setLibrary("md", md)
 
-    eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(feedPlugin, {
+        type: "rss",
+        outputPath: "/feed.xml",
+        collection: {
+            name: "all",
+            limit: 0,
+        },
+        metadata: {
+            language: "es",
+            title: "La web de Nuzkito",
+            subtitle: "Blog sobre desarrollo de software.",
+            base: config().url,
+            author: {
+                name: "Nuzkito",
+            }
+        }
+    })
+    eleventyConfig.addPlugin(syntaxHighlight)
     eleventyConfig.addPlugin(timeToRead, {
         language: 'es',
-    });
-    eleventyConfig.addPassthroughCopy("blog.css");
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("favicon.ico");
-    eleventyConfig.addPassthroughCopy("fonts");
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("CNAME");
-};
+    })
+    eleventyConfig.addPassthroughCopy("blog.css")
+    eleventyConfig.addPassthroughCopy("css")
+    eleventyConfig.addPassthroughCopy("favicon.ico")
+    eleventyConfig.addPassthroughCopy("fonts")
+    eleventyConfig.addPassthroughCopy("images")
+    eleventyConfig.addPassthroughCopy("CNAME")
+}
